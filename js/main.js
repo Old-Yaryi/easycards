f8ad75a73fd3d0e0c0ef5fb20a413626471db0dd
Old-Yaryi <kinomir-v@yandex.ru>
2023-06-09 17:37:57 +0300

    'popup_nah'

--- js/main.js
+++ js/main.js
@@ -11,3 +11,22 @@ for (item of faqBlock) {
     }
   })
 }
+const popupTrigger = document.querySelector (".button-select.bank");
+const popupBlock = document.querySelector('.popup')
+const popupClosed = document.querySelector('.popup-inner__close')
+popupTrigger.addEventListener('click', function(){
+  popupBlock.classList.add('active')
+  
+})
+popupClosed.addEventListener('click', function(){
+  popupBlock.classList.toggle('active')
+})
+popupBlock.addEventListener('click', function(e){
+  let actMenu = document.querySelector(".popup-item");
+   if (e.target !== actMenu && !actMenu.contains(e.target)) {                    
+     popupBlock.classList.remove('active');
+  }
+  console.log('preved')
+})
+
+
